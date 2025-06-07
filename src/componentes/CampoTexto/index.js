89b49dc23b498c2f617b5000181b53fa01267b14
italoesilva <italoliveira95@hotmail.com>
2025-06-07 13:03:36 -0300

    Meu projeto React

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+
+const placeholderModificada = `${props.placeholder}...`
+
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+                </label>
+
+            <input placeholder={`${props.placeholder}...`}></input>
+
+        </div>
+
+
+    )
+}
+
+export default CampoTexto
