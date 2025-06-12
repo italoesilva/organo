68b6cdb9483a9cac71584ad86ae8fd3f9fc217ed
italoesilva <italoliveira95@hotmail.com>
2025-06-12 20:16:10 -0300

    fiz card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+        return (<div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/italoesilva.png' alt='Ítalo Oliveira' />
+
+            </div>
+            <div className='rodape'>
+                <h4>Ítalo</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>)
+
+
+}
+
+export default Colaborador
